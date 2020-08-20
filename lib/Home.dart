5adbe50c17f2d274024d87856c498008ccdf27d1
lmas <30lmas09@gmail.com>
2020-08-20 17:35:30 +0530

    'added_home_page'

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,4 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,8 +11,49 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text("hello"),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.blue[200],
+      body: Container(
+        margin: const EdgeInsets.only(top: 50),
+        child: Center(
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 200,
+              width: 200,
+              child: FlareActor("assets/icon.flr",color: Colors.white,animation: 'Untitled',fit: BoxFit.scaleDown,),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 50),
+              child: Text("Welcome to your Personal Self Care Assistant",style: TextStyle(color: Colors.white,fontSize: 30,fontWeight:FontWeight.bold),textAlign: TextAlign.center,),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 150),
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(width: 1,color: Colors.white,),
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: Center(
+                child: Text("Hi, Nicky_AI",textAlign: TextAlign.center,style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),),
+              )
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 50),
+              child:Text("Login to your Account!", style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold,)),
+            ),
+          ],
+        ),
+        )
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:Your_personal/Home.dart';
-import 'package:flare_splash_screen/flare_splash_screen.dart';
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,15 +31,23 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+   @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 10), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Home(),
+          ));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SplashScreen.navigate(
-        name:'./assets/icon.flr', 
-        next: (_)=>Home(),
-        until: ()=>Future.delayed(Duration(seconds: 5)),
-        startAnimation: '1',
-      ),
+      backgroundColor: Colors.blue[200],
+      body:FlareActor("assets/icon.flr", alignment:Alignment.center,color: Colors.white,animation: 'Untitled',fit: BoxFit.scaleDown,),
     );
   }
 }
